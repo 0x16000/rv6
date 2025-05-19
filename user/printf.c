994f548565c2ba7ccbc9409c61eef3b0a6f6d54d
0x16000 <openbsd.gnu@gmail.com>
2025-05-19 11:48:26 +0200

    let printf understand %c

--- user/printf.c
+++ user/printf.c
@@ -65,8 +65,9 @@ vprintf(int fd, const char *fmt, va_list ap)
       }
     } else if(state == '%'){
       c1 = c2 = 0;
-      if(c0) c1 = fmt[i+1] & 0xff;
-      if(c1) c2 = fmt[i+2] & 0xff;
+      if(fmt[i+1]) c1 = fmt[i+1] & 0xff;
+      if(fmt[i+2]) c2 = fmt[i+2] & 0xff;
+
       if(c0 == 'd'){
         printint(fd, va_arg(ap, int), 10, 1);
       } else if(c0 == 'l' && c1 == 'd'){
@@ -93,6 +94,8 @@ vprintf(int fd, const char *fmt, va_list ap)
         i += 2;
       } else if(c0 == 'p'){
         printptr(fd, va_arg(ap, uint64));
+      } else if(c0 == 'c'){
+        putc(fd, va_arg(ap, int));
       } else if(c0 == 's'){
         if((s = va_arg(ap, char*)) == 0)
           s = "(null)";
@@ -101,42 +104,15 @@ vprintf(int fd, const char *fmt, va_list ap)
       } else if(c0 == '%'){
         putc(fd, '%');
       } else {
-        // Unknown % sequence.  Print it to draw attention.
+        // Unknown % sequence. Print it to draw attention.
         putc(fd, '%');
         putc(fd, c0);
       }
-
-#if 0
-      if(c == 'd'){
-        printint(fd, va_arg(ap, int), 10, 1);
-      } else if(c == 'l') {
-        printint(fd, va_arg(ap, uint64), 10, 0);
-      } else if(c == 'x') {
-        printint(fd, va_arg(ap, int), 16, 0);
-      } else if(c == 'p') {
-        printptr(fd, va_arg(ap, uint64));
-      } else if(c == 's'){
-        s = va_arg(ap, char*);
-        if(s == 0)
-          s = "(null)";
-        while(*s != 0){
-          putc(fd, *s);
-          s++;
-        }
-      } else if(c == 'c'){
-        putc(fd, va_arg(ap, uint));
-      } else if(c == '%'){
-        putc(fd, c);
-      } else {
-        // Unknown % sequence.  Print it to draw attention.
-        putc(fd, '%');
-        putc(fd, c);
-      }
-#endif
       state = 0;
     }
   }
 }
+
 
 void
 fprintf(int fd, const char *fmt, ...)
